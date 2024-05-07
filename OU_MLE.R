a28bbfc30e06f995db79db61a5178420286c65c6
AlvaroMaza <alvaromazamontesinos@gmail.com>
2024-05-07 13:51:25 +0200

    pairwise plot, delta example

--- OU_MLE.R
+++ OU_MLE.R
@@ -1,116 +1,73 @@
 library(goffda)
 
-
-NegLogLikOU <- function (theta, delta, X) {
+negative_log_likelihood <- function(params, delta_t, X) {
+  alpha <- params[1]
+  sigma_sq <- params[2]
+  mu <- params[3]
+  
   n <- length(X)
+  term1 <- -n/2 * log(sigma_sq / (2 * alpha))
+  term2 <- -0.5 * n * log1p(-exp(-2 * alpha * delta_t))
   
-  alpha <- exp(theta[1])
-  mu  <- theta[2]
-  sigma <- exp(theta[3])
-  
-  term1 <- -n/2 * log(sigma / (2 * alpha))
-  term2 <- -0.5 * n * log1p(-exp(-2 * alpha * delta))
-  term3 <- -(alpha / sigma) * sum(((X[2:n] - mu - (X[1:(n-1)] - mu) * exp(-alpha * delta))^2) / (1 - exp(-2 * alpha * delta)))
+  sum <- 0
+  for (i in 2:n) {
+    nominator <- (X[i] - mu - (X[i - 1] - mu) * exp(-alpha * delta_t))^2
+    denominator <- 1 - exp(-2 * alpha * delta_t)
+    sum <- sum + nominator/denominator
+  }
+  term3 <- -(alpha / sigma_sq) * sum
   
   log_likelihood_value <- term1 + term2 + term3
+  print(c(term1, term2, term3))
   return(- log_likelihood_value)
-  
 }
 
 
-# Function to compute confidence intervals using Fisher information
-compute_CI <- function(theta_hat, fisher_info, n, alpha = 0.05) {
 
+
+delta_t <- 0.01 
+T <- 1
+
+initial_params <- c(1, 1, 1) 
+lower_bounds <- c(0.05, 0.05, -Inf)  
+upper_bounds <- c(Inf, Inf, Inf)    
+
+result_list <- list()
+num_iterations <- 50
+
+
+for (i in 1:num_iterations) {
+  X <- r_ou(n = 1, t = seq(0, T, len = T/delta_t), x0 = 20, mu = 20, sigma = 2, alpha = 4)$data
   
-  standard_errors <- sqrt(diag(solve(fisher_info))) / sqrt(n)
-  
-  z_critical <- qnorm(1 - alpha / 2)
-  
-  margin_of_error <- z_critical * standard_errors
-  lower_bounds <- theta_hat - margin_of_error
-  upper_bounds <- theta_hat + margin_of_error
-  
-  lower_bounds[1] <- exp(lower_bounds[1])
-  theta_hat[1] <- exp(theta_hat[1])
-  upper_bounds[1] <- exp(upper_bounds[1])
-  lower_bounds[3] <- exp(lower_bounds[3])
-  theta_hat[3] <- exp(theta_hat[3])
-  upper_bounds[3] <- exp(upper_bounds[3])
-  
-  results <- data.frame(
-    Estimate = theta_hat,
-    Lower_CI = lower_bounds,
-    Upper_CI = upper_bounds
-  )
-  return(results)
+  result <- optim(method = "L-BFGS-B", par = initial_params, fn = negative_log_likelihood, delta_t = delta_t, X = X,
+                  lower = lower_bounds, upper = upper_bounds)
+  result_list[[i]] <- result$par
 }
 
 
-est.OU <- function (X, delta) {
-  
-  
-  theta <- c(log(1), mean(X), log(1))
-  
-  n <- length(X)
-  optim.results <- optim( par = theta, fn = NegLogLikOU, delta = delta, X = X, hessian=TRUE)
-  theta_hat <- optim.results$par
-  hessian <- optim.results$hessian
-  
-  alpha <- exp(theta_hat[1])
-  mu  <- theta_hat[2]
-  sigma <- exp(theta_hat[3])
-  fisher_info<-hessian/n
-  
-  ci <- compute_CI(theta_hat, fisher_info, n)
-  return( ci )
-}
-
-
-X <- r_ou(n = 1, t = seq(0, 1, len = 201), x0 = 10, mu = 15, sigma = 1, alpha = 4)$data
-X <- as.vector(X)
-delta <- 0.005
-
-result <- est.OU(X, delta)
+result_matrix <- do.call(rbind, result_list)
+means <- colMeans(result_matrix)
+print(means)
 
 
 
 
+sd_val <- num_iterations/(2*(16)^2)
+hist(result_matrix[, 2], main = paste("Histogram of sigma^2"),
+     xlab = paste("Sigma^2"), breaks = 20, probability = T, ylim = c(0, 1))
+curve(dnorm(x, mean = 16, sd = sd_val), add = TRUE, col = "blue")
 
 
-############### TEST CI ###############
 
-# Set parameters
-n_iterations <- 100
-percentage_alpha <- numeric(n_iterations)
-percentage_mu <- numeric(n_iterations)
-percentage_sigma <- numeric(n_iterations)
-
-T <- 10
-delta <- 0.01
-
-mu <- 15
-sigma <- 1
-alpha <- 4
+hist(result_matrix[, 1], main = paste("Histogram of alpha"),
+     xlab = paste("Alpha"), breaks = 20)
 
 
-# Loop through iterations
-for (i in 1:n_iterations) {
-  X <- r_ou(n = 1, t = seq(0, T, len = (T/delta + 1)), x0 = 15, mu = mu, sigma = sigma, alpha = alpha)$data
-  X <- as.vector(X)
 
-  result <- est.OU(X, delta)
-  percentage_alpha[i] <- result[1, 2] < alpha & alpha < result[1, 3]
-  percentage_mu[i] <- result[2, 2] < mu & mu < result[2, 3]
-  percentage_sigma[i] <- result[3, 2] < sigma & sigma < result[3, 3]
-}
+hist(result_matrix[, 3], main = paste("Histogram of mu"),
+     xlab = paste("Mu"), breaks = 20)
 
 
-# Calculate percentages
-percentage_alpha <- mean(percentage_alpha)
-percentage_mu <- mean(percentage_mu)
-percentage_sigma <- mean(percentage_sigma)
 
-print(paste("Percentage for alpha:", percentage_alpha))
-print(paste("Percentage for mu:", percentage_mu))
-print(paste("Percentage for sigma:", percentage_sigma))
+
 
